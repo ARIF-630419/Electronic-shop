2516fc3444903ff1822ce4185d11597b697f0dff
ARIF-630419 <ariful25-013@diu.edu.bd>
2022-08-16 03:19:29 +0600

    project success

---
+++ frontend/src/component/about/About.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Footer from "../../Footer";
+import Header from "../Home/Header";
+import Loading from "../../more/Loader";
+import MetaData from "../../more/Metadata";
+import "./About.css";
+import BottomTab from "../../more/BottomTab";
+import bg2 from "../../Assets/bg2.jpg";
+
+const About = () => {
+    const { loading } = useSelector(
+        (state) => state.profile
+      );
+  return (
+    <>
+    {loading ? <Loading /> : 
+    <>
+    <MetaData title="About" />
+    <div>
+    <Header />
+    <div
+      style={{
+        width: "90%",
+        margin: "0px auto",
+      }}
+    >
+      <div className="about__page">
+        {/* 1st verse */}
+        <div className="row flex">
+          <div className="col__2">
+            <img src={bg2} />
+          </div>
+          <div className="col__2">
+            <div className="meta">
+              <span
+                style={{
+                  fontSize: "40px",
+                  fontWeight: "700",
+                  lineHeight: "1.2",
+                }}
+              >
+                Welcome to Electronic Shop
+              </span>
+              <p className="justify">
+              The online Electronics shop has been developed to override the problems prevailing in the practicing manual system. This software is supported to eliminate and in some cases reduce the hardships faced by this existing system. Moreover, this system is designed for the particular need of the company to carry out operations in a smooth and effective manner.
+              </p>
+              <p className="justify">
+              The application is reduced as much as possible to avoid errors while entering the data. It also provides error message while entering invalid data. No formal knowledge is needed for the user to use this system. Thus by this all it proves it is user friendly. Online Electronic shop, as described above, can assist the user to concentrate on their other activities rather to concentrate on the record keeping. Thus it will help organization in better utilization of resources.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* 2nd verse */}
+        <div className="second">
+          <div className="heading">
+            <h2>What We Provide?</h2>
+          </div>
+          <div className="row flex">
+            <div className="col__3">
+                <div style={{
+                    padding:"10px",
+                    border:"1px solid rgb(0 0 0 / 14%)",
+                    minHeight:"230px"
+                }}>
+                <div className="flex align__items__center justify__content__center image">
+              <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-1.svg" />
+                </div>
+              <span>Best Prices & Offers</span>
+              <p>
+              Text messages that include discounts are the bread and butter of most SMS marketing strategies. Here’s how to write discount messages that drive more revenue.
+              </p>
+              </div>
+            </div>
+            <div className="col__3">
+                <div style={{
+                    padding:"10px",
+                    border:"1px solid rgb(0 0 0 / 14%)",
+                    minHeight:"230px"
+                }}>
+                <div className="flex align__items__center justify__content__center image">
+              <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-2.svg" />
+                </div>
+              <span>Best For Trust & Quality</span>
+              <p>
+              Quality Electronics guarantees competitive pricing & full satisfaction 
+with every product we sell.
+              </p>
+              </div>
+            </div>
+            <div className="col__3">
+                <div style={{
+                    padding:"15px",
+                    border:"1px solid rgb(0 0 0 / 14%)",
+                    minHeight:"230px"
+                }}>
+                <div className="flex align__items__center justify__content__center image">
+              <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-3.svg" />
+                </div>
+              <span>Fast Delivery System</span>
+              <p>
+              Give your customers 2-day delivery & ship the same day no matter your size.
+              </p>
+              </div>
+            </div>
+
+
+            <div className="col__3">
+                <div style={{
+                    padding:"15px",
+                    border:"1px solid rgb(0 0 0 / 14%)",
+                    minHeight:"230px"
+                }}>
+                <div className="flex align__items__center justify__content__center image">
+              <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-4.svg" />
+                </div>
+              <span>Easy Returns Service</span>
+              <p>
+              Complete order fulfilment and returns management services
+Processing customer returns is often an enormous headache, 
+consuming valuable time and resources.
+              </p>
+              </div>
+            </div>
+
+            <div className="col__3">
+                <div style={{
+                    padding:"15px",
+                    border:"1px solid rgb(0 0 0 / 14%)",
+                    minHeight:"230px"
+                }}>
+                <div className="flex align__items__center justify__content__center image">
+              <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-5.svg" />
+                </div>
+              <span>100% satisfication</span>
+              <p>
+              Customer satisfaction is defined as a measurement that determines 
+how happy customers are with a company's products, services, 
+and capabilities.
+              </p>
+              </div>
+            </div>
+            
+            <div className="col__3">
+                <div style={{
+                    padding:"15px",
+                    border:"1px solid rgb(0 0 0 / 14%)",
+                    minHeight:"230px"
+                }}>
+                <div className="flex align__items__center justify__content__center image">
+              <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-6.svg" />
+                </div>
+              <span>Great Daily Deal</span>
+              <p>
+              We constantly monitor phone and laptop prices and these are 
+some of the best discounts we've spotted.
+              </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+  <BottomTab />
+  </>
+    }
+    </>
+  );
+};
+
+export default About;
